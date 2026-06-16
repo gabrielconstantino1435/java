dab481e3cec88b62c52e424cdec14baa99871139
Gabriel Constantino <guest.jb@alunos.sc.senac.br>
2026-06-15 21:10:56 -0300

    um é igual a um

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -14,7 +14,9 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-   System.out.println("olá mundo");
+if (1==1){
+       System.out.println("um é igual a um");
+}
     }
 
     
